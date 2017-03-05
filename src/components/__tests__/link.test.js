3981edd195e8977ca99c6f94d23aafe890586ff8
Joacle <joaquinfs@hotmail.com>
2017-03-05 15:28:11 +0100

    Configure jest to test react components

---
+++ src/components/__tests__/link.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Link from '../Link';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
+
+// test('Link exists', () => {
+//   const wrapper = shallow(<Link />);
+//   expect(wrapper.find(Link)).toBe.have.length(1);
+// });
+
